3295a947d9a1160628460237f19643af7f06fc96
oricha <oricha@gmail.com>
2023-11-12 12:05:52 +0100

    Added Security

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ $> docker build --tag=message-server:latest .
 
 ```bash
 $> docker run -p8887:8888 message-server:latest
-```
+``` 
--- src/main/java/com/kmuniz/storeapi/domain/configuration/BeanConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.kmuniz.storeapi.domain.configuration;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class BeanConfiguration {
-
-}

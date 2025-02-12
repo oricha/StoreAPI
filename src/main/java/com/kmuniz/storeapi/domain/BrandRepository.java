161c6a946022679fefec1558cfce6744250f6ecd
Karel <oricha@gmail.com>
2025-02-12 23:17:10 +0100

    removed Brand

--- src/main/java/com/kmuniz/storeapi/domain/BrandRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kmuniz.storeapi.domain;
-
-import com.kmuniz.storeapi.domain.entity.Brand;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BrandRepository extends JpaRepository<Brand, Long> {
-}
